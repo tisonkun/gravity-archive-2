5d2e19811b856907a4677de92760ac0c426040e8
tison <wander4096@gmail.com>
2022-04-24 21:56:44 +0800

    feat: watch commits
    Signed-off-by: tison <wander4096@gmail.com>

--- contributors.py
+++ contributors.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python3
 
+# Copyright 2022 tison <wander4096@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import argparse
 import os
 import dotenv
 import pandas as pd
 import requests
 
-"""
-GitHub's contributor graph shows only the first 100 contributors.
-"""
 
 def contributors_to_pandas(contributors):
     result = [{
@@ -30,10 +41,9 @@ if __name__ == "__main__":
     headers['Authorization'] = os.getenv('GITHUB_TOKEN')
 
     page = 1
-    repo = args.repo
     contributors = []
     while True:
-        result = requests.get(url=f'https://api.github.com/repos/{repo}/contributors?per_page=100&page={page}', headers=headers)
+        result = requests.get(url=f'https://api.github.com/repos/{args.repo}/contributors?per_page=100&page={page}', headers=headers)
         links = result.links
         contributors += result.json()
         if 'next' not in links:
---
+++ watch_commits.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+# Copyright 2022 tison <wander4096@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from collections import namedtuple
+import os
+
+import dotenv
+import requests
+
+
+if __name__ == "__main__":
+    dotenv.load_dotenv()
+
+    headers = {}
+    headers['Accept'] = 'application/vnd.github.v3+json'
+    headers['Authorization'] = os.getenv('GITHUB_TOKEN')
+
+    Watcher = namedtuple('Watcher', ['repo', 'commit'])
+    watchers = [
+        Watcher(repo='stretchr/testify', commit='77977386932ab1866a4b9556a7af1ae347531786')
+    ]
+
+    exit_code = 0
+    for watcher in watchers:
+        result = requests.get(url=f'https://api.github.com/repos/{watcher.repo}/commits?per_page=1', headers=headers)
+        commit = result.json()[0]['sha']
+
+        if commit != watcher.commit:
+            print(f'{watcher.repo} has been pushed new commit: {commit}, last commit: {watcher.commit}')
+            exit_code = 1
+
+    exit(exit_code)
